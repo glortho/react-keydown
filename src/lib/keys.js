af4bc99feb78b75eaa928983608157cafc6a1f88
glortho <jed@dataverse.me>
2015-10-09 15:53:39 -0400

    add alphabet to keys lib

--- src/lib/keys.js
+++ src/lib/keys.js
@@ -1,11 +1,13 @@
-export default {
+const Keys = {
   TAB:   9,
   ENTER: 13,
   LEFT:  37,
   UP:    38,
   RIGHT: 39,
   DOWN:  40,
-  J:     74,
-  K:     75,
-  SLASH: 191 // => '/'
+  SLASH: 191
 };
+
+'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split( '' ).forEach( ( letter, index ) => Keys[letter] = index + 65 );
+
+export default Keys;
