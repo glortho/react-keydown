3206e59ca154a19f686d69e95d612cd0d5869ace
glortho <jed@dataverse.me>
2015-10-08 21:21:21 -0400

    document listeners module

--- src/lib/listeners.js
+++ src/lib/listeners.js
@@ -1,3 +1,7 @@
+/**
+ * @module listeners
+ *
+ */
 import React from 'react';
 
 /**
@@ -5,29 +9,73 @@ import React from 'react';
  * 
  */
 
+// dict for class prototypes => { bindings, instances }
 const _handlers      = new Map();
+
+// the currently focused instance that should receive key presses
 let _focusedInstance = null;
+
+// flag for whether click listener has been bound to document
 let _clicksBound     = false;
+
+// flag for whether keydown listener has been bound to document
 let _keysBound       = false;
 
+/**
+ * _addInstance
+ *
+ * @access private
+ * @param {object} target Instantiated class that extended React.Component
+ * @return {set} The set of instances for the passed in class
+ */
 function _addInstance( target ) {
   return getBinding( target.constructor.prototype ).instances.add( target );
 }
 
+/**
+ * _deleteInstance
+ *
+ * @access private
+ * @param {object} target Instantiated class that extended React.Component
+ * @return {boolean} The value set.has( target ) would have returned prior to deletion
+ */
 function _deleteInstance( target ) {
   return getBinding( target.constructor.prototype ).instances.delete( target );
 }
 
+/**
+ * _findFocused
+ *
+ * @access private
+ * @param {object} data Criteria to use for finding the focused node
+ * @param {object} data.instance The instantiated React.Component that is
+ * a candidate for being focuse
+ * @param {object} data.target The DOM node from the click event
+ * @return {boolean} Success or failure in matching the node to the event target
+ */
 function _findFocused( { target, instance } ) {
   const node = React.findDOMNode( instance );
   return target === node || node.contains( target );
 }
 
+/**
+ * _focus
+ *
+ * @access private
+ * @param {object} instance Instantiated class that extended React.Component, to be focused to receive keydown events
+ */
 function _focus( instance ) {
   _focusedInstance = instance;
   return instance ? _bindKeys() : _unbindKeys();
 }
 
+/**
+ * _handleClick
+ *
+ * @access private
+ * @param {object} event The click event object
+ * @param {object} event.target The DOM node from the click event
+ */
 function _handleClick( { target } ) {
   const findFocused = instance => _findFocused( { target, instance } );
   let focusedInstance = null;
@@ -38,6 +86,13 @@ function _handleClick( { target } ) {
   _focus( focusedInstance );
 }
 
+/**
+ * _handleKeyDown
+ *
+ * @access private
+ * @param {object} event The keydown event object
+ * @param {number} event.which The key code (which) received from the keydown event
+ */
 function _handleKeyDown( { which } ) {
   const { bindings } = getBinding( _focusedInstance.constructor.prototype );
   bindings.forEach( ( fn, keys ) => (
@@ -45,6 +100,11 @@ function _handleKeyDown( { which } ) {
   ));
 }
 
+/**
+ * _bindKeys
+ *
+ * @access private
+ */
 function _bindKeys() {
   if ( !_keysBound ) {
     document.addEventListener( 'keydown', _handleKeyDown );
@@ -52,6 +112,11 @@ function _bindKeys() {
   }
 }
 
+/**
+ * _unbindKeys
+ *
+ * @access private
+ */
 function _unbindKeys() {
   if ( _keysBound ) {
     document.removeEventListener( 'keydown', _handleKeyDown );
@@ -59,6 +124,11 @@ function _unbindKeys() {
   }
 }
 
+/**
+ * _bindClicks
+ *
+ * @access private
+ */
 function _bindClicks() {
   if ( !_clicksBound ) {
     document.addEventListener( 'click', _handleClick );
@@ -66,6 +136,11 @@ function _bindClicks() {
   }
 }
 
+/**
+ * _unbindClicks
+ *
+ * @access private
+ */
 function _unbindClicks() {
   if ( _clicksBound && ![ ..._handlers ].some( ( [, { instances } ] ) => instances.size ) ) {
     document.removeEventListener( 'click', _handleClick );
@@ -79,10 +154,26 @@ function _unbindClicks() {
  *
  */
 
+/**
+ * getBinding
+ *
+ * @access public
+ * @param {object} target Class used as key in dict of bindings and instances
+ * @return {object} The object containing bindings and instances for the given class
+ */
 function getBinding( target ) {
   return _handlers.get( target );
 }
 
+/**
+ * setBinding
+ *
+ * @access public
+ * @param {object} args All arguments necessary to set the binding
+ * @param {array} args.keys Key codes that should trigger the fn
+ * @param {function} args.fn The callback to be triggered when given keys are pressed
+ * @param {object} args.target The decorated class
+ */
 function setBinding( { keys, fn, target } ) {
   let handler = getBinding( target );
   if ( !handler ) {
@@ -91,6 +182,11 @@ function setBinding( { keys, fn, target } ) {
   handler.bindings.set( keys, fn );
 }
 
+/**
+ * onMount
+ *
+ * @access public
+ */
 function onMount() {
   _bindClicks();
   _addInstance( this );
@@ -99,6 +195,11 @@ function onMount() {
   setTimeout(() => _focus( this ), 0);
 }
 
+/**
+ * onUnmount
+ *
+ * @access public
+ */
 function onUnmount() {
   _deleteInstance( this );
   _unbindClicks();
--- src/lib/method_decorator.js
+++ src/lib/method_decorator.js
@@ -1,21 +1,23 @@
 import { getBinding, setBinding, onMount, onUnmount } from './listeners';
 
 function methodWrapper( { target, descriptor, keys } ) {
-  const { componentDidMount, componentWillUnmount } = target;
-  const fn = descriptor.value;
-  
+
   if ( !getBinding( target ) ) {
+
+    const { componentDidMount, componentWillUnmount } = target;
+    
     target.componentDidMount = function() {
       onMount.call( this );
       if ( componentDidMount ) return componentDidMount.call( this );
     };
+
     target.componentWillUnmount = function() {
       onUnmount.call( this );
       if ( componentWillUnmount ) return componentWillUnmount.call( this );
     };
   }
 
-  setBinding( { keys, fn, target } );
+  setBinding( { keys, target, fn: descriptor.value } );
 
   return descriptor;
 }
