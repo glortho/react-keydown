59106e9a1c408f4bd3891d25cc2ecaa7a60d33bc
glortho <jed@dataverse.me>
2015-10-08 21:37:33 -0400

    document class decorator

--- src/lib/class_decorator.js
+++ src/lib/class_decorator.js
@@ -1,7 +1,19 @@
+/**
+ * @module componentWrapper
+ *
+ */
 import React from 'react';
 
 import { setBinding, onMount, onUnmount } from './listeners';
 
+/**
+ * componentWrapper
+ *
+ * @access public
+ * @param {object} WrappedComponent React component class to be wrapped
+ * @param {array} [keys] The key(s) bound to the class
+ * @return {object} The higher-order function that wraps the decorated class
+ */
 function componentWrapper( WrappedComponent, keys = null ) {
 
   class KeyBoardHelper extends React.Component {
@@ -22,6 +34,7 @@ function componentWrapper( WrappedComponent, keys = null ) {
     }
 
     handleKeyDown( event ) {
+      // to simulate a keypress, set the event and then clear it in the callback
       this.setState( { event }, () => this.setState( { event: null } ) );
     }
 
