55fd746c0d47767abe0d6bf31c8ca27659d4ff18
glortho <jed@dataverse.me>
2015-10-11 14:41:20 -0400

    compile latest

--- dist/lib/keys.js
+++ dist/lib/keys.js
@@ -18,14 +18,24 @@
   });
   exports.allKeys = allKeys;
   var Keys = {
+    backspace: 8,
     tab: 9,
     enter: 13,
+    'return': 13,
+    space: 32,
     left: 37,
     up: 38,
     right: 39,
     down: 40,
-    slash: 191
+    slash: 191,
+    '/': 191,
+    backslash: 220,
+    '\\': 220
   };
+
+  '0123456789'.split('').forEach(function (num, index) {
+    return Keys[num] = index + 48;
+  });
 
   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').forEach(function (letter, index) {
     Keys[letter] = index + 65;
