53f8d51b415a8c4afb32e8e5745342243f8a6efb
glortho <jed@dataverse.me>
2015-10-12 12:00:23 -0400

    minor code clean up for key match

--- src/lib/match_keys.js
+++ src/lib/match_keys.js
@@ -6,8 +6,10 @@ function matchKeys( { keySet: { key, modifiers = [] }, event } ) {
   let keysMatch = false;
   if ( key === event.which ) {
     const evtModKeys = modKeys.filter( modKey => event[ `${modKey}Key` ] ).sort();
-    keysMatch = modifiers.length === evtModKeys.length && 
-      modifiers.every( ( modKey, index ) => evtModKeys[ index ] );
+    keysMatch = (
+      modifiers.length === evtModKeys.length && 
+      modifiers.every( ( modKey, index ) => evtModKeys[ index ] )
+    );
   }
   return keysMatch;
 }
