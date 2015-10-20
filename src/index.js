9ec1e022b8ca7867fe412107fdc5c25199127afa
glortho <jed@dataverse.me>
2015-10-20 10:30:52 -0400

    minor alignment fix for index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 export { default, keydownScoped } from './decorators';
 
 // setBinding - only useful if you're not going to use decorators
-export { setBinding }             from './store';
+export { setBinding } from './store';
 
 // Keys - use this to find key codes for strings. for example: Keys.j, Keys.enter
-export { default as Keys }        from './lib/keys';
+export { default as Keys } from './lib/keys';
