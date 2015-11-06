6242fafed8b3cf27bda701377a5ec880a81bb582
glortho <jed@dataverse.me>
2015-11-05 22:20:11 -0500

    begin implementation of tests

---
+++ tests/store-test.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+
+import Store from '../src/store';
+
+test( 'activate', t => {
+  const fixture = {};
+  let msg;
+
+  msg = 'instances initially empty';
+  t.ok( Store.isEmpty(), msg );
+
+  msg = 'instances size matches after activate';
+  Store.activate( fixture );
+  t.equal( Store.getInstances().size, 1, msg );
+
+  msg = 'concats array';
+  Store.activate( [ {}, {} ] );
+  t.equal( Store.getInstances().size, 3, msg );
+
+  msg = 'caps instances with null when nothing passed in';
+  Store.activate( null );
+  t.ok( Store.getInstances().has( null ), msg );
+
+  msg = 'removes null next time valid args passed in';
+  Store.activate( {} );
+  t.notOk( Store.getInstances().has( null ), msg );
+
+  msg = 'moves most recently activated member to end';
+  Store.activate( fixture );
+  const arr = [ ...Store.getInstances() ];
+  t.equal( arr.indexOf( fixture ), arr.length - 1, msg );
+
+  t.end();
+});
+
