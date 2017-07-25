d418d614c5a92e527e8b38f175ff0febc6824ccd
glortho <jed.verity@digitalglobe.com>
2017-07-25 10:39:17 -0400

    remove build artifacts

--- es/lib/uuid.js
+++
@@ -1,9 +0,0 @@
-// Counter being incremented. JS is single-threaded, so it'll Just Work™.
-var __counter = 1;
-
-/**
- * Returns a process-wide unique identifier.
- */
-export default function uuid() {
-  return "uid-" + __counter++;
-}
